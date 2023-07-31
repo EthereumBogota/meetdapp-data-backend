f85101c6601ee7831ff9965146c4f784cd024c22
oscar50 <oscar@whaleandjaguar.co>
2023-07-30 22:07:46 -0500

    feat: install sql_lachemy

--- main.py
+++ main.py
@@ -1,4 +1,21 @@
+import os
+from dotenv import load_dotenv
 from fastapi import FastAPI
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+load_dotenv()
+
+
+SQLALCHEMY_DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL")
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
 
 
 app = FastAPI()
@@ -6,6 +23,6 @@ app.title = 'Meet Dapp API'
 app.version = '0.0.1'
 
 
-@app.get('/',tags=["home"])
+@app.get('/', tags=["home"])
 def onboarding(): 
     return "Welcome Meet Dapp dev"
