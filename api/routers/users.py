83d29b7f78f541037fc366b491f3fcf598857b40
rookiecol <juan.diegam@hotmail.com>
2023-09-03 16:43:16 -0500

    :sparkles: First user registration system

--- api/routers/users.py
+++ api/routers/users.py
@@ -1,45 +1,76 @@
-'''
-Include all the routes starting at /user/
-'''
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException, status
 from sqlmodel import Session
-from api.services.database import get_session 
+from api.services.database import get_session
+from api.models.models import User
+
 
 router = APIRouter(
     prefix="/users",
     tags=["users"],
-    # dependencies=[Depends(get_token_header)],
-    # responses={404: {"description": "Not found"}},
 )
 
-@router.get("/")
-async def read_users(*, db: Session = Depends(get_session)):
-    '''
-    TODO: not created yet
-    '''
-    return [{"wallet": "Rick"}, {"wallet": "Morty"}]
+@router.get("/", response_model=list[User])
+async def read_users(db: Session = Depends(get_session)):
+    """
+    Get a list of all users.
+    """
+    users = db.query(User).all()
+    return users
 
-@router.get("/me")
-async def read_user_me(*, db: Session = Depends(get_session)):
-    '''
-    TODO: not created yet
-    '''
-    return {"wallet": "fakecurrentuser"}
+@router.get("/{wallet}", response_model=User)
+async def read_user(wallet: str, db: Session = Depends(get_session)):
+    """
+    Get user details by wallet address.
+    """
+    user = db.query(User).filter(User.wallet == wallet).first()
+    if user:
+        return user
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail="User not found",
+    )
 
-@router.post("/{wallet}")
-async def read_user(*, db: Session = Depends(get_session), wallet: str):
-    '''
-    TODO: not created yet
-    '''
-    return {"wallet": wallet}
+@router.post("/", response_model=User)
+async def create_user(user: User, db: Session = Depends(get_session)):
+    """
+    Create a new user.
+    """
+    db.add(user)
+    db.commit()
+    db.refresh(user)
+    return user
 
+@router.put("/{wallet}", response_model=User)
+async def update_user(wallet: str, updated_user: User, db: Session = Depends(get_session)):
+    """
+    Update user details by wallet address.
+    """
+    existing_user = db.query(User).filter(User.wallet == wallet).first()
+    if not existing_user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="User not found",
+        )
 
-@router.get("/{wallet}")
-async def read_user(*, db: Session = Depends(get_session), wallet: str):
-    '''
-    TODO: not created yet
-    '''
-    return {"wallet": wallet}
+    for key, value in updated_user.dict().items():
+        setattr(existing_user, key, value)
 
+    db.commit()
+    db.refresh(existing_user)
+    return existing_user
 
+@router.delete("/{wallet}", response_model=User)
+async def delete_user(wallet: str, db: Session = Depends(get_session)):
+    """
+    Delete user by wallet address.
+    """
+    user = db.query(User).filter(User.wallet == wallet).first()
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="User not found",
+        )
 
+    db.delete(user)
+    db.commit()
+    return user
